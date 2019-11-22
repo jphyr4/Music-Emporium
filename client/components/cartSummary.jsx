8ea7e5fec7483c4582d048af35a127a995f27cb0
Jake Hassler <jphyr4@mail.missouri.edu>
2019-11-22 19:11:21 +0000

    cartsummary front end

--- client/components/cartSummary.jsx
+++ client/components/cartSummary.jsx
@@ -3,10 +3,17 @@ import CartSummaryItem from './cartSummaryItem';
 
 function CartSummary(props) {
   const itemMap = props.cartItemProp.map((item, index) => <CartSummaryItem key={index} item={item}/>);
+  const totalPrice = props.cartItemProp.reduce((total, value) => (total + value.price), 0);
+  const total = totalPrice / 100;
   return (
     <div>
       {itemMap},
-      <button onClick={() => props.backToCat('catalog', {})} type="button" className="btn btn-outline-secondary w-25">Back to Catalog</button>
+      <div className="text-center">
+        <h1>Total: ${total.toFixed(2)}</h1>
+      </div>
+      <div className="text-center">
+        <button onClick={() => props.backToCat('catalog', {})} type="button" className="btn btn-outline-secondary w-25 pagination-centered">Back to Catalog</button>
+      </div>
     </div>
   );
 }
